82bd7c8e4a6def9f0f7b266ac36ca9c88d9fd4ea
Lucchetto <lucchetto.tie@live.com>
2020-11-25 13:29:10 +0100

    Add support to display array resources
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/resourcesviewer/util/ResourcesUtils.kt
+++ app/src/main/java/com/zhenxiang/resourcesviewer/util/ResourcesUtils.kt
@@ -6,9 +6,10 @@ import com.zhenxiang.resourcesviewer.Resource
 
 class ResourcesUtils {
     companion object {
-        val resTypes = arrayOf("dimen", "string", "bool", "integer", "color")
-        var value : String? = null;
+        val resTypes = arrayOf("dimen", "string", "bool", "integer", "color", "array")
+
         fun getResourceValue(resource : Resource, context: Context) : String? {
+            var value : String?;
             val resources = context.packageManager.getResourcesForApplication(resource.packageName)
             value = when (resource.resType) {
                 "dimen" -> "${resources.getDimensionPixelSize(resource.resId).toString()} px"
@@ -16,10 +17,38 @@ class ResourcesUtils {
                 "bool" -> resources.getBoolean(resource.resId).toString()
                 "integer" -> resources.getInteger(resource.resId).toString()
                 "color" -> "#${Integer.toHexString(resources.getColor(resource.resId))}"
+                "array" -> System.lineSeparator() + getArrayResourceValues(resource, context).joinToString(System.lineSeparator())
                 else -> null
             }
             return value
         }
+
+        fun getArrayResourceValues(resource : Resource, context: Context) : Array<String> {
+            var values : Array<String> = emptyArray();
+            var isIntArray : Boolean;
+            val resources = context.packageManager.getResourcesForApplication(resource.packageName)
+            if (resources.getStringArray(resource.resId).isNotEmpty()) {
+                isIntArray = resources.getStringArray(resource.resId)[0] == null
+                if (isIntArray) {
+                    values = convertIntToStringArray(resources.getIntArray(resource.resId))
+                } else {
+                    values = resources.getStringArray(resource.resId)
+                }
+            }
+            return values
+        }
+
+        fun convertIntToStringArray(intArray : IntArray) : Array<String> {
+            if (intArray.isEmpty()) {
+                return emptyArray()
+            }
+            val stringArray : Array<String> = Array(intArray.size) { "" }
+            for (i in intArray.indices) {
+                stringArray[i] = intArray[i].toString()
+            }
+            return stringArray
+        }
+
         fun getResourceByName(resName: String, packageName : String, context: Context) : Resource? {
             for (resType in resTypes) {
                 val resId = getResourceIdByName(resName, resType, packageName, context)
