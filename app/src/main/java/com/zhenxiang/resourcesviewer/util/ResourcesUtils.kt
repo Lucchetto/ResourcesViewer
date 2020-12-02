f26a8b29bf3fe2cfa1821dacd8cb4b5438a12041
Lucchetto <lucchetto.tie@live.com>
2020-12-02 18:12:30 +0100

    ResourcesUtils: show original dimen value and unit
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/resourcesviewer/util/ResourcesUtils.kt
+++ app/src/main/java/com/zhenxiang/resourcesviewer/util/ResourcesUtils.kt
@@ -1,29 +1,58 @@
 package com.zhenxiang.resourcesviewer.util
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.content.res.Resources
+import android.util.TypedValue
 import com.zhenxiang.resourcesviewer.Resource
+
 
 class ResourcesUtils {
     companion object {
         val resTypes = arrayOf("dimen", "string", "bool", "integer", "color", "array")
 
-        fun getResourceValue(resource : Resource, context: Context) : String? {
+        fun getResourceValue(resource: Resource, context: Context) : String? {
             var value : String?;
             val resources = context.packageManager.getResourcesForApplication(resource.packageName)
             value = when (resource.resType) {
-                "dimen" -> "${resources.getDimensionPixelSize(resource.resId).toString()} px"
+                "dimen" -> {
+                    val outValue = TypedValue()
+                    resources.getValue(resource.resId, outValue, true)
+                    val pixelDimen = resources.getDimensionPixelSize(resource.resId)
+                    val rawDimen = TypedValue.complexToFloat(outValue.data)
+                    "${pixelDimen}px ${rawDimen}${getDimenUnitString(getComplexUnit(outValue.data))}"
+                }
                 "string" -> resources.getString(resource.resId)
                 "bool" -> resources.getBoolean(resource.resId).toString()
                 "integer" -> resources.getInteger(resource.resId).toString()
                 "color" -> "#${Integer.toHexString(resources.getColor(resource.resId))}"
-                "array" -> System.lineSeparator() + getArrayResourceValues(resource, context).joinToString(System.lineSeparator())
+                "array" -> System.lineSeparator() + getArrayResourceValues(
+                    resource,
+                    context
+                ).joinToString(
+                    System.lineSeparator()
+                )
                 else -> null
             }
             return value
         }
 
-        fun getArrayResourceValues(resource : Resource, context: Context) : Array<String> {
+        fun getComplexUnit(data: Int): Int {
+            return TypedValue.COMPLEX_UNIT_MASK and (data shr TypedValue.COMPLEX_UNIT_SHIFT)
+        }
+
+        fun getDimenUnitString(unit: Int) : String {
+            return when(unit) {
+                TypedValue.COMPLEX_UNIT_DIP -> "dp"
+                TypedValue.COMPLEX_UNIT_IN -> "in"
+                TypedValue.COMPLEX_UNIT_MM -> "mm"
+                TypedValue.COMPLEX_UNIT_PX -> "px"
+                TypedValue.COMPLEX_UNIT_SP -> "sp"
+                TypedValue.COMPLEX_UNIT_PT -> "pt"
+                else -> ""
+            }
+        }
+
+        fun getArrayResourceValues(resource: Resource, context: Context) : Array<String> {
             var values : Array<String> = emptyArray();
             var isIntArray : Boolean;
             val resources = context.packageManager.getResourcesForApplication(resource.packageName)
@@ -38,7 +67,7 @@ class ResourcesUtils {
             return values
         }
 
-        fun convertIntToStringArray(intArray : IntArray) : Array<String> {
+        fun convertIntToStringArray(intArray: IntArray) : Array<String> {
             if (intArray.isEmpty()) {
                 return emptyArray()
             }
@@ -49,7 +78,7 @@ class ResourcesUtils {
             return stringArray
         }
 
-        fun getResourceByName(resName: String, packageName : String, context: Context) : Resource? {
+        fun getResourceByName(resName: String, packageName: String, context: Context) : Resource? {
             for (resType in resTypes) {
                 val resId = getResourceIdByName(resName, resType, packageName, context)
                 if (resId != 0) {
@@ -59,7 +88,12 @@ class ResourcesUtils {
             return null
         }
 
-        fun getResourceByName(resName: String, resType : String, packageName : String,  context: Context) : Resource? {
+        fun getResourceByName(
+            resName: String,
+            resType: String,
+            packageName: String,
+            context: Context
+        ) : Resource? {
             val resId = getResourceIdByName(resName, resType, packageName, context)
             if (resId != 0) {
                 return Resource(resId, resType, packageName)
@@ -67,7 +101,12 @@ class ResourcesUtils {
             return null
         }
 
-        fun getResourceIdByName(resName: String, resType : String, packageName : String, context : Context) : Int {
+        fun getResourceIdByName(
+            resName: String,
+            resType: String,
+            packageName: String,
+            context: Context
+        ) : Int {
             val resources = context.packageManager.getResourcesForApplication(packageName)
             return resources.getIdentifier(resName, resType, packageName)
         }
